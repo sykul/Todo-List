77e9ea403c694850d91ed049a9b32fb0a32a7bf8
sykul <luke.flatley@outlook.com>
2024-05-23 18:47:39 +0200

    rename todo.js to task.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*  title, description, dueDate, priority, notes, done */
 import './style.css';
 import 'date-fns';
-import { TaskObject } from './todo.js'
+import { TaskObject } from './task.js'
 
 const task1 = new TaskObject('example name');
 
