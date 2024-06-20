326d4c4f34c9251d62046a5eb9f72dcc68306f24
sykul <luke.flatley@outlook.com>
2024-06-20 22:25:52 +0200

    test task and card creation

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import { displayProjects } from './loadhomepage.js';
 import { displayTasks } from './loadprojectpage.js';
 import { createTaskCard } from './ui.js';
 import { addToLocalStorage, retrieveFromLocalStorage } from './localstoragefunctions.js'
+import { te } from 'date-fns/locale';
 
-const testObject = new TaskObject('testname', 'testproject', "id" + Math.random().toString(16).slice(2));
+const testObject = new TaskObject('testname', 'defaultProject', "id" + Math.random().toString(16).slice(2));
+const testObject1 = new TaskObject('testname1', 'defaultProject', "id" + Math.random().toString(16).slice(2));
 
 addToLocalStorage(testObject);
+addToLocalStorage(testObject1);
 let retrievedObject = retrieveFromLocalStorage(testObject);
 
 displayProjects();
-console.log(`testObject: ${testObject.taskName}`);
+createTaskCard(retrievedObject);
 createTaskCard(testObject);
-console.log(`retrievedObject: ${retrievedObject.taskName}`);
+createTaskCard(testObject1);
