f7559b8895fa5287012c9d5a2b56cd5eb36cb2c9
sykul <luke.flatley@outlook.com>
2024-06-21 11:05:34 +0200

    add event listener to new cards

--- src/ui.js
+++ src/ui.js
@@ -5,6 +5,8 @@ function expandTask() {
 function createTaskCard(taskObject) {
     const taskCard = document.createElement('div')
     taskCard.classList.add('task-card', `${taskObject.taskIndex}`);
+    taskCard.addEventListener('click', (e) => console.log(e.target))
+
     const content = document.querySelector('.content');
     content.appendChild(taskCard);
 }
