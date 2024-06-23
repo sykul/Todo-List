c1300bd1f4899d12bd440c4096d547dc090f1826
sykul <luke.flatley@outlook.com>
2024-06-23 14:31:08 +0200

    add event listeners and capture target

--- src/ui.js
+++ src/ui.js
@@ -4,8 +4,12 @@ function expandTask() {
 
 function createTaskCard(taskObject) {
     const taskCard = document.createElement('div')
-    taskCard.classList.add('task-card', `${taskObject.taskIndex}`);
-    taskCard.addEventListener('click', (e) => console.log(e.target))
+    taskCard.classList.add('task-card');
+    taskCard.setAttribute('id', `${taskObject.taskIndex}`);
+    taskCard.addEventListener('click', (e) => {
+        console.log(e.target.id);
+        console.log(taskObject.taskIndex);
+    });
 
     const content = document.querySelector('.content');
     content.appendChild(taskCard);
