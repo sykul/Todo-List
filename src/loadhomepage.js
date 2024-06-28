7e4736c65e5961bf4d757966a185e43c4850870d
sykul <luke.flatley@outlook.com>
2024-06-28 15:30:21 +0200

    Add textbox to modal

--- src/loadhomepage.js
+++ src/loadhomepage.js
@@ -1,23 +1,42 @@
 import { el } from "date-fns/locale";
 
-function displayProjects() {
-    const element = document.querySelector('.content');
-    const bodyElement = document.querySelector('body');
+const element = document.querySelector('.content');
+const bodyElement = document.querySelector('body');
 
+function createHeading() {
     const heading = document.createElement('h1');
     heading.textContent = "Project Overview Page";
     element.appendChild(heading);
+}
 
-    const addProjectModal = document.createElement('div');
-    addProjectModal.classList.add('modal', 'hidden');
-    bodyElement.appendChild(addProjectModal);
-
+function createAddProjectButton() {
     const addProjectButton = document.createElement('button');
+    const addProjectModal = document.querySelector('.modal');
     addProjectButton.classList.add('add-project-button');
     addProjectButton.addEventListener('click', () => {
         addProjectModal.classList.toggle('hidden');
-    })
+    });
     bodyElement.appendChild(addProjectButton);
+}
+
+function createModal() {
+    const addProjectModal = document.createElement('div');
+    addProjectModal.classList.add('modal', 'hidden');
+
+    const textBox = document.createElement('input');
+    textBox.type = 'text';
+    addProjectModal.appendChild(textBox);
+
+    bodyElement.appendChild(addProjectModal);
+}
+
+function displayProjects() {
+
+    createHeading();
+
+    createModal();
+
+    createAddProjectButton();
 
 }
 
