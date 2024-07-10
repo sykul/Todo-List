efd2ab6629f19ee6f5bccdd2147b147ebc96ed5c
sykul <luke.flatley@outlook.com>
2024-07-10 14:39:14 +0200

    remove testing function calls from index.js

--- src/index.js
+++ src/index.js
@@ -9,19 +9,6 @@ import { addToLocalStorage, retrieveFromLocalStorage } from './localstoragefunct
 
 const projects = [];
 
-
-const project1 = new ProjectObject('project1');
-projects.push(project1);
-const task1 = new TaskObject('task1','project1');
-const task2 = new TaskObject('task2','project1');
-project1.addTaskToProject(task1);
-project1.addTaskToProject(task2);
-const project2 = new ProjectObject('project2');
-projects.push(project2);
-
 displayProjectPage();
-const projectCard = document.querySelector('.project-card');
-projectCard.dispatchEvent(new Event('click'));
-
 
 export { projects }
